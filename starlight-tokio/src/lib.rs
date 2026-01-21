c111f9eb841ed2a69473e6bfd3d14147a07cd26f
Nguyễn Đức Thông <43933486+DianaSensei@users.noreply.github.com>
2026-01-21 13:21:16 +0700

    update

--- starlight-tokio/src/lib.rs
+++ starlight-tokio/src/lib.rs
@@ -1,11 +1,5 @@
 mod runnable_service;
 
-pub use tokio::main;
-pub use tokio::{sync, net, task, signal};
-pub use tokio::{join, spawn, select};
-pub use tokio_util::sync::CancellationToken;
-pub use async_trait::async_trait;
-
-pub use anyhow;
+pub use tokio;
 
 pub use runnable_service::StarlightService;
--- starlight-tokio/src/runnable_service.rs
+++ starlight-tokio/src/runnable_service.rs
@@ -1,7 +1,7 @@
-use async_trait::async_trait;
-use tokio_util::sync::CancellationToken;
+use std::sync::Arc;
+use tokio::sync::watch;
+use tokio::task::JoinHandle;
 
-#[async_trait]
 pub trait StarlightService: Send + Sync + 'static {
-    async fn run(&self, shutdown: CancellationToken ) -> anyhow::Result<()>;
+    fn run(&self, shutdown_tx: Arc<watch::Sender<bool>>, shutdown_rx: watch::Receiver<bool>, ) -> JoinHandle<()>;
 }
